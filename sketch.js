254234590cfabe61270dbc64126b335c469755ef
Austin Beck <abeck7@lsu.edu>
2019-09-19 12:03:37 -0500

    second commit

--- sketch.js
+++ sketch.js
@@ -1,27 +1,35 @@
 function setup(){
     createCanvas(windowWidth, windowHeight);
-    background(62, 23, 64);
+    background(11, 10, 8);
 
-    for(var x = 9999; x > 0; x = x - 15){
-        print(x);
-        //translate(width / 2, 0);
-        //translate(p5.Vector.fromAngle(millis() / 1000, 4));
-        fill(62, 23, 64);
-        stroke(206, 209, 65);
-        rect(x, 0, 40, 3000);
+    var r1 = random(width);
+    var r2 = random(height);
+
+    // dot array background
+    for(var a = 0; a <= height - 20; a +=10){
+        for(var b = 0; b <= width - 20; b +=10){
+            fill(62, 23, 64);
+            ellipse(b, a, 4, 4);
+            //draw line to center
+            stroke(111, 124, 179);
+            strokeWeight(0.4);
+            line(b, a, r1, r2);
+        }
     }
-    
-    fill("beige");
-    circle(random(width), random(height), random(width / 2));
 
+    // the moon
+    fill("beige");
+    circle(r1, r2, random(width / 2));
+
+    // the pillars
     for(var i = 10000; i > 0; i = i - 20){
         print(i);
-        circle(i, i, 10);
         fill(55, 142, 116);
         stroke(16, 28, 29);
-        rect(random(i), random(i), 30, i, 10);
+        rect(random(i), random(i), 16, i, 10);
     }
 
+    // the ground?
     for(var u = 5000; u > 0; u = u - 2){
         print(u);
         fill(15, 159, 221);
@@ -30,8 +38,6 @@ function setup(){
 }
 
 function draw(){
-    
-    print(mouseX, mouseY);
 
     var redC1 = 150;
     var greenC1 = 0;
